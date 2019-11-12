6b8660f5bc49e135c9677a74b170f2436bee99de
Fauzi <fauzi.adithya@gmail.com>
2019-11-12 22:15:08 +0700

    Make text-input to support multi input

--- text-input/script.js
+++ text-input/script.js
@@ -1,10 +1,12 @@
-var input = document.getElementById("textinput");
-var placeholder = document.getElementsByClassName("placeholder");
+var inputs = document.getElementsByClassName("textinput");
 
-input.addEventListener( 'focusin', e => {
-    placeholder[0].classList.add('top');
-});
 
-input.addEventListener( 'focusout', e => {
-    if (e.path[0].value == '') placeholder[0].classList.toggle('top');
-});
+for (input of inputs){
+    let placeholder = input.getElementsByClassName("placeholder");
+    input.addEventListener( 'focusin', e => {
+        placeholder[0].classList.add('top');
+    });
+    input.addEventListener( 'focusout', e => {
+        if (e.path[0].value == '') placeholder[0].classList.toggle('top');
+    });
+}
